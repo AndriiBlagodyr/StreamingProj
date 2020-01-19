a56a8814a2b0459d6a52646ba1329caf43c2affd
Mr_Anderson <andrii.blagodyr@gmail.com>
2020-01-19 16:27:02 +0300

    feat(CMT-4)Redux setup

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
+import { fetchComments } from '../actions';
 import logo from '../logo.svg';
 
-export const MainPage = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit 
+export const MainPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchComments());
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit 
 {' '}
 <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
 
 MainPage.displayName = 'MainPage';
